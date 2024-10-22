ab86b184b301a86f6cf4eb6d2ca12d694484043b
Lora Berger <berger.lora@gmail.com>
2024-10-21 19:04:27 -0700

    Pycharm adds

---
+++ module01/A01_LBerger.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment01
+# Desc: This assignment demonstrates using constants, variables, and print()
+# Change Log: (Who, When, What)
+#   Lora Berger,2024-10-13,Created Script
+# ------------------------------------------------------------------------------------------ #
+
+CLASS_NAME = 'Python 100'
+first_name = None
+last_name = None
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+print(first_name + ' ' + last_name + ' is registered for ' + CLASS_NAME)
